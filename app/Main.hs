e24912e7c80615d5736408be515f8935375695ba
Fredrik Liljegren <fiddur.liljegren@auth0.com>
2017-04-10 10:48:08 +0200

    Testing cabala...

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, NoImplicitPrelude #-}
+
 module Main where
 
-import Lib
+import ClassyPrelude
+
+import Database.EventStore
+import Data.Aeson
 
 main :: IO ()
-main = someFunc
+main = do
+  con <- connect defaultSettings { s_retry = keepRetrying } (Static "localhost" 1113)
+
+  let js  = object ["toto" .= True]
+      evt = createEvent "testing" Nothing (withJson js)
+
+  fork $ forever $ do
+    (_ :: ByteString) <- getLine
+    sendEvent con "strim" anyVersion evt
+
+  fork . forever $ handle (\(_ :: SubscriptionClosed) -> threadDelay 5000000) (mkSub con >>= process)
+
+  waitTillClosed con
+
+  where
+    mkSub c = do
+      putStrLn "making a subscription"
+      sub <- subscribe c "strim" True
+
+      waitConfirmation sub
+      putStrLn "sub confirmed"
+
+      return sub
+
+    process sub = do
+      putStrLn "processing..."
+
+      forever $ do
+        n <- nextEvent sub
+        print (recordedEventNumber $ resolvedEventOriginal n)
+        
