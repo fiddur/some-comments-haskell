628ae08ccd5137290b8c538e3e2201ae13f7439d
Fredrik Liljegren <fiddur.liljegren@auth0.com>
2017-04-10 11:45:57 +0200

    More trial and terror in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,38 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, NoImplicitPrelude #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE QuasiQuotes               #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module Main where
-
-import ClassyPrelude
 
 import Database.EventStore
 import Data.Aeson
 
-main :: IO ()
-main = do
+import Yesod
+import Data.Aeson
+import Data.Maybe (catMaybes)
+import Database.EventStore
+import Control.Concurrent.Async
+
+{- 
+
+{-# LANGUAGE NoImplicitPrelude         #-}
+
+import ClassyPrelude
+import Prelude -- (getLine)
+
+testevent :: IO ()
+testevent = do
   con <- connect defaultSettings { s_retry = keepRetrying } (Static "localhost" 1113)
 
   let js  = object ["toto" .= True]
       evt = createEvent "testing" Nothing (withJson js)
 
   fork $ forever $ do
-    (_ :: ByteString) <- getLine
+    foo <- getLine
+    -- (_ :: ByteString) <- getLine
     sendEvent con "strim" anyVersion evt
 
   fork . forever $ handle (\(_ :: SubscriptionClosed) -> threadDelay 5000000) (mkSub con >>= process)
@@ -38,4 +55,33 @@ main = do
       forever $ do
         n <- nextEvent sub
         print (recordedEventNumber $ resolvedEventOriginal n)
-        
+-}
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/events EventsR GET
+|]
+
+instance Yesod HelloWorld
+
+getLanguageEvents :: [ResolvedEvent]
+getLanguageEvents = do
+  -- TODO: Should connect in main and pass the connection...
+  eventstore <- connect defaultSettings (Static "127.0.0.1" 1113)
+  rs <- readStreamEventsForward eventstore "languages" 0 100 False >>= waitAsync
+  case rs of
+    ReadSuccess sl -> do
+      -- let events = catMaybes $ fmap resolvedEventDataAsJson $ sliceEvents sl
+      sliceEvents sl
+    e -> error $ "Read failure: " ++ show e
+
+getEventsR :: Handler Value
+getEventsR = do
+  events <- liftIO getLanguageEvents
+  return events
+
+
+main :: IO ()
+main = do
+  warp 3000 HelloWorld
